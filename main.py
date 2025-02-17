ff2b4a25519050483e8c77565e63856022169767
geo_prepod_pc <maksikbet13@gmail.com>
2025-02-17 20:59:37 +0300

    second made

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt6.QtGui import QPixmap
 import requests
+from PyQt6.QtCore import Qt
 
 
 class MAPAPI(QMainWindow):
@@ -12,6 +13,11 @@ class MAPAPI(QMainWindow):
         self.initUI()
 
     def image_maker(self, coords, scale):
+        if scale > 21:
+            scale = 21
+        elif scale <= 0:
+            scale = 1
+        scale = int(scale)
         server_address = 'http://static-maps.yandex.ru/1.x/?'
         ll_spn = f'll={coords}&z={scale}'
         # Готовим запрос.
@@ -41,6 +47,18 @@ class MAPAPI(QMainWindow):
         self.image = QLabel(self)
         self.image.resize(600, 600)
         self.image.setPixmap(self.pixmap)
+
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp and self.scale < 21:
+            self.scale += 1
+        elif event.key() == Qt.Key.Key_PageDown and self.scale > 0:
+            self.scale -= 1
+        else:
+            return
+
+        self.image_maker(self.cords, self.scale)
+        self.image.setPixmap(QPixmap('map.png'))
 
 
     def closeEvent(self, event):
